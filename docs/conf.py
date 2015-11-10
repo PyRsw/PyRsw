43385c7e5c5c66f1e60d78f7011e94d620959892
Francis Poulin <fpoulin@uwaterloo.ca>
2015-11-09 22:00:39 -0500

    Changed the html theme to be ReadTheDocs and authors to PyRsw

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pysw'
-copyright = u'2015, Francis Poulin and Ben Storer'
-author = u'Francis Poulin and Ben Storer'
+copyright = u'2015, PyRsw team'
+author = u'PyRsw team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -109,9 +109,13 @@ todo_include_todos = False
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'alabaster'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -203,7 +207,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pyswdoc'
+htmlhelp_basename = 'PyRswdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -225,8 +229,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'pysw.tex', u'pysw Documentation',
-   u'Francis Poulin and Ben Storer', 'manual'),
+  (master_doc, 'PyRsw.tex', u'PyRsw Documentation',
+   u'PyRsw Team', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -255,7 +259,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'pysw', u'pysw Documentation',
+    (master_doc, 'PyRsw', u'PyRsw Documentation',
      [author], 1)
 ]
 
@@ -269,8 +273,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'pysw', u'pysw Documentation',
-   author, 'pysw', 'One line description of project.',
+  (master_doc, 'PyRsw', u'PyRsw Documentation',
+   author, 'PyRsw', 'One line description of project.',
    'Miscellaneous'),
 ]
 
