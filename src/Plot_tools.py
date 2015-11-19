Fixed the tranpose bug. Now trying to understand why we have a small time step and why things become numerically unstable.

src/Plot_tools.py
@@ -25,7 +25,7 @@ def update_anim(sim):
 
     if sim.Nx > 1 and sim.Ny > 1:
         for L in range(sim.Nz):
-            sim.Qs[L].set_array(np.ravel(sim.soln.h[:sim.Nx-1,:sim.Ny-1,L].T))
+            sim.Qs[L].set_array(np.ravel(sim.soln.u[:sim.Nx-1,:sim.Ny-1,L]))
             sim.Qs[L].changed()
     else:
         # Update u
@@ -46,7 +46,7 @@ def update_anim(sim):
         sim.axs[2].relim()
         sim.axs[2].autoscale_view()
 
-    plt.pause(0.01)
+    plt.pause(2) #0.01)
     plt.draw()
 
 # Create Hovmuller plot at end of anim simulation
@@ -129,22 +129,23 @@ def initialize_plots_animsave(sim):
     fig.suptitle('t = 0')
 
     if sim.Nx > 1 and sim.Ny > 1:
-        x = sim.x/1e3
-        y = sim.y/1e3
+        x = sim.X/1e3
+        y = sim.Y/1e3
         Qs  = []
         axs = []
         for L in range(sim.Nz):
             plt.subplot(1,sim.Nz,L+1)
             axs += [plt.gca()]
-            Qs += [plt.pcolormesh(x,y,sim.soln.h[:,:,L].T, cmap='viridis')]
+            Qs += [plt.pcolormesh(x,y,sim.soln.u[:,:,L], cmap='viridis')]
             plt.colorbar()
             try:
-                plt.contour(x,y,sim.soln.h[:,:,-1].T)
+                plt.contour(x,y,sim.soln.u[:,:,-1])
             except:
                 pass
             plt.axis('tight')
             plt.gca().set_aspect('equal')
-
+            plt.pause(2.0)
+            
     else:
         Qs  = [[],[],[]]
         axs = []

src/PyRsw.py
@@ -115,7 +115,7 @@ class Simulation:
             self.Y = 0.
 
         #FJP: fix this later      
-        self.X, self.Y = np.meshgrid(self.x,self.y)
+        self.X, self.Y = np.meshgrid(self.x,self.y,indexing='ij')
         self.X -= self.Lx/2.
         self.Y -= self.Ly/2.
         self.F = self.f0 + self.beta*self.Y

src/example_BickleyJet.py
@@ -10,7 +10,7 @@ import sys
 sim = Simulation()  # Create a simulation object
 
 # Geometry and Model Equations
-sim.geomx       = 'walls'       # Geometry Types: 'periodic' or 'walls'
+sim.geomx       = 'periodic'       # Geometry Types: 'periodic' or 'walls'
 sim.geomy       = 'walls'
 sim.stepper     = Step.AB2         # Time-stepping algorithm: Euler, AB2, RK4
 sim.method      = 'Spectral'       # Numerical method: 'Spectral'
@@ -20,19 +20,19 @@ sim.flux_method = Flux.spectral_sw # Flux method: spectral_sw is only option cur
 # Specify paramters
 sim.Lx  = 200e3          # Domain extent               (m)
 sim.Ly  = 200e3          # Domain extent               (m)
-sim.Nx  = 128             # Grid points in x
-sim.Ny  = 128             # Grid points in y
+sim.Nx  = 64             # Grid points in x
+sim.Ny  = 64             # Grid points in y
 sim.Nz  = 1               # Number of layers
 sim.g   = 9.81            # Gravity                     (m/sec^2)
 sim.f0  = 1.e-4           # Coriolis                    (1/sec)
 sim.beta = 0e-10          # Coriolis beta               (1/m/sec)
-sim.cfl = 0.1            # CFL coefficient             (m)
+sim.cfl = 0.05            # CFL coefficient             (m)
 sim.Hs  = [100.]          # Vector of mean layer depths (m)
 sim.rho = [1025.]         # Vector of layer densities   (kg/m^3)
 sim.end_time = 24.*hour   # End Time                    (sec)
 
 # Plotting parameters
-sim.plott   = 1.*minute  # Period of plots
+sim.plott   = 10.*minute  # Period of plots
 sim.animate = 'Anim'      # 'Save' to create video frames,
                           # 'Anim' to animate,
                           # 'None' otherwise
@@ -54,16 +54,19 @@ for ii in range(sim.Nz):  # Set mean depths
 # Gaussian initial conditions
 Ljet = 20e3            # Jet width
 amp  = 0.1             # Elevation of free-surface in basic state
-sim.soln.u[:,:,0]  =  sim.g*amp/(sim.f0*Ljet)/(np.cosh(sim.Y/Ljet)**2)
 sim.soln.h[:,:,0] += -amp*np.tanh(sim.Y/Ljet)
+sim.soln.u[:,:,0]  =  sim.g*amp/(sim.f0*Ljet)/(np.cosh(sim.Y/Ljet)**2)
+sim.soln.u[:,:,0] +=  1e-3*np.exp(-(sim.Y/Ljet)**2)*np.random.randn(sim.Nx,sim.Ny)
+#sim.soln.h[:,:,0] +=  amp*np.tanh(sim.X/Ljet)
+#sim.soln.v[:,:,0]  =  sim.g*amp/(sim.f0*Ljet)/(np.cosh(sim.X/Ljet)**2)
 
 #FJP: plot solution
 #FJP: LSA
-plt.figure()
-plt.clf()
-plt.pcolormesh(sim.X, sim.Y, sim.soln.u[:,:,0], cmap='seismic')
-plt.colorbar()
-plt.show()
+#plt.figure()
+#plt.clf()
+#plt.pcolormesh(sim.X, sim.Y, sim.soln.u[:,:,0], cmap='seismic')
+#plt.colorbar()
+#plt.show()
 #plt.plot(sim.Y[:,0]/1e3, -amp*np.tanh(sim.Y[:,0]/Ljet),'-ob')
 #plt.show()
 #plt.plot(sim.Y[:,0]/1e3,  sim.g*amp/(sim.f0*Ljet)/(np.cosh(sim.Y/Ljet)**2),'-ob')