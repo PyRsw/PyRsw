f5a7a77a7c084bc418b2e0b497ac5c09c30ecaa0
Ben Storer <bastorer@uwaterloo.ca>
2015-11-23 15:41:01 -0500

    Change CFL

--- src/example_1D_adjust.py
+++ src/example_1D_adjust.py
@@ -7,11 +7,12 @@ from PyRsw import Simulation
 from constants import minute, hour, day
 import sys
 
-sim = Simulation()  # Create a simulation object
+# Create a simulation object
+sim = Simulation() 
 
 # Geometry and Model Equations
 sim.geomx       = 'walls'       # Geometry Types: 'periodic' or 'walls'
-sim.geomy       = 'periodic'
+sim.geomy       = 'walls'
 sim.stepper     = Step.AB3         # Time-stepping algorithm: Euler, AB2, RK4
 sim.method      = 'Spectral'       # Numerical method: 'Spectral'
 sim.dynamics    = 'Nonlinear'      # Dynamics: 'Nonlinear' or 'Linear'
@@ -19,21 +20,21 @@ sim.flux_method = Flux.spectral_sw # Flux method: spectral_sw is only option cur
 
 # Specify paramters
 sim.Lx  = 4000e3          # Domain extent               (m)
-sim.Ly  = 4000e3          # Domain extent               (m)
+sim.Ly  = 6000e3          # Domain extent               (m)
 sim.Nx  = 1                 # Grid points in x
-sim.Ny  = 128             # Grid points in y
+sim.Ny  = 512             # Grid points in y
 sim.Nz  = 1               # Number of layers
 sim.g   = 9.81            # Gravity                     (m/sec^2)
 sim.f0  = 1.e-4           # Coriolis                    (1/sec)
-sim.beta = 0e-10          # Coriolis beta parameter     (1/m/sec)
-sim.cfl = 0.2             # CFL coefficient             (m)
+sim.beta = 4e-11          # Coriolis beta parameter     (1/m/sec)
+sim.cfl = 0.1             # CFL coefficient             (m)
 sim.Hs  = [100.]          # Vector of mean layer depths (m)
 sim.rho = [1025.]         # Vector of layer densities   (kg/m^3)
 sim.end_time = 14*24.*hour   # End Time                    (sec)
 
 # Plotting parameters
 sim.plott   = 20.*minute  # Period of plots
-sim.animate = 'Anim'      # 'Save' to create video frames,
+sim.animate = 'Save'      # 'Save' to create video frames,
                           # 'Anim' to animate,
                           # 'None' otherwise
                          
@@ -52,14 +53,15 @@ for ii in range(sim.Nz):  # Set mean depths
     sim.soln.h[:,:,ii] = sim.Hs[ii]
 
 # Gaussian initial conditions
-W  = 200.e3                # Width
+W  = 200.e3               # Width
 amp = 1.                  # Amplitude
 if sim.Ny==1:
     sim.soln.h[:,:,0] += amp*np.exp(-(sim.X)**2/(W**2))
 elif sim.Nx==1:
     sim.soln.h[:,:,0] += amp*np.exp(-(sim.Y)**2/(W**2))
 
-sim.run()                # Run the simulation
+# Run the simulation
+sim.run()
 
 
 # Hovmuller plot
