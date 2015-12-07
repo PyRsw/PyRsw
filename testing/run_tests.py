7277d927a349154dff2e6648ba91457036871af7
bastorer <bastorer@uwaterloo.ca>
2015-12-07 18:50:59 -0500

    Update .travis.yml

---
+++ testing/run_tests.py
@@ -0,0 +1,18 @@
+
+# test_does_1d_run
+execfile('test_does_1d_run/test_periodic.py')
+execfile('test_does_1d_run/test_walls.py')
+
+# test_does_2d_run
+execfile('test_does_2d_run/test_periodic_periodic.py')
+execfile('test_does_2d_run/test_periodic_walls.py')
+execfile('test_does_2d_run/test_walls_periodic.py')
+execfile('test_does_2d_run/test_walls_walls.py')
+
+# test_steppers
+execfile('test_steppers/test_Euler.py')
+execfile('test_steppers/test_AB2.py')
+execfile('test_steppers/test_AB3.py')
+
+# test_1d_accuracy
+execfile('test_1d_accuracy/test_linear_waves.py')
